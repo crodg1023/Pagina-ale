05c40e3505d1b8b653c71fc586b5b88ec6d38cce
Trench <127424548+crodg1023@users.noreply.github.com>
2023-06-20 15:48:48 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const img1 = document.querySelector('#img1');
+const img2 = document.querySelector('#img2');
+const img3 = document.querySelector('#img3');
+
+img1.addEventListener('click', () => {
+    location.replace('20 razones.html');
+})
+img2.addEventListener('click', () => {
+    location.replace('vuelta al sol 3D.html');
+})
+img3.addEventListener('click', () => {
+    location.replace('20 estrellitas.html');
+})
